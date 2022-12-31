faf938ef146620ce94b90b78e8145a4c3efe9b79
2000031519_Aneesh <2000031519@kluniversity.in>
2022-12-31 14:34:38 +0530

    hello to jenkines

---
+++ new_31.java
@@ -0,0 +1,10 @@
+// jikens java_code
+
+class HelloWorld {
+    public static void main(String[] args) {
+    int a=10,b=29;
+    int c=a+b;
+        System.out.println("Hello, World!"); 
+         System.out.println(c); 
+    }
+}
